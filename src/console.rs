18ead63496432e1a69f108b579295103f255b5d6
dyk <359743984@qq.com>
2021-07-11 17:07:16 +0800

    init: initial

---
+++ src/console.rs
@@ -0,0 +1,35 @@
+use crate::base::syscall;
+use core::fmt::{self, Write};
+
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+pub fn console_putchar(c: usize) {
+    syscall(SBI_CONSOLE_PUTCHAR, [c, 0, 0]);
+}
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            console_putchar(c as usize);
+        }
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+#![feature(llvm_asm)]
+#![feature(global_asm)]
+
+mod base;
+mod console;
+
+mod lang_items;
+mod sbi;
+use sbi::sbi_call;
+
+global_asm!(include_str!("entry.asm"));
+
+const SBI_SHUTDOWN: usize = 8;
+pub fn shutdown() {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+}
+
+#[allow(dead_code)]
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) })
+}
+
+#[no_mangle]
+extern "C" fn rust_main() {
+    println!("hello wolrd");
+    shutdown()
+}
